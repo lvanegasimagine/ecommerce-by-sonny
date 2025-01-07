1d14f80730b0382322849420568b99470b847cdc
Luis Vanegas <lvanegas1429@gmail.com>
2025-01-07 12:02:31 -0600

    searchparams fix

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -5,9 +5,7 @@ import React from 'react';
 
 export default async function SearchPage({
     searchParams,
-}: {
-    searchParams: { query: string };
-}) {
+}: { searchParams: Promise<{ query: string }> }) {
     const { query } = await searchParams;
     console.log("🚀 ~ query:", query)
     const products = await searchProductsByName(query);
